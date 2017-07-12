17fa70e48d878a3acf67f9ee9bcea7a0d77cd4a5
Stage <stage@mac0164.irap.omp.eu>
2017-07-12 16:45:08 +0200

    Test about the functions and the units

--- Codes/Source_finie.py
+++ Codes/Source_finie.py
@@ -249,8 +249,8 @@ number_bin_eps = 20.0
 
 # Parameters for the code
 L = 20 * conv_l                         # the distance to the gamma-source (cm)
-zb = -10 * conv_l                        # position along the line of sight nearly the star (cm)
-b = 0 * conv_l                          # impact parameter (cm)
+zb = 10 * conv_l                        # position along the line of sight nearly the star (cm)
+b = 5 * conv_l                          # impact parameter (cm)
 D_star =  np.sqrt(b**2 + (L - zb)**2)   # distance to the star (from us) (cm)
 D_gamma = np.sqrt(b**2 + L**2)          # distance between the star and the gamma-source (cm)
 R = 0.5 * conv_l                        # radius of the star (express in Rsun)
