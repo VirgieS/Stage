0877863b57b28ea6dc49f2231ea0ee0adb6b31ec
Stage <stage@mac0164.irap.omp.eu>
2017-07-12 16:48:38 +0200

    All importants graphics from the differents tests

---
+++ Codes/Conversion_factor.py
@@ -0,0 +1,12 @@
+#Conversion factors
+
+# Energy
+
+kevJ = 1.602e-16            # from keV to J
+Jerg = 1e7                  # from J to erg
+ergkev = 1/(kevJ * Jerg)    # from erg to keV
+
+# Length
+
+kpccm = 3.085678e21         # from kpc to cm
+aucm = 1.45979e13           # from au to cm
---
+++ Codes/Physical_constants.py
@@ -0,0 +1,12 @@
+#Physical constants
+
+#in cgs
+
+cl = 2.99792458e10              # cm/s
+hp = 6.6260755e-27              # erg.s
+kb = 1.380658e-16               # erg/K
+sb = 5.67051                    # erg/cm^2/K^4
+r0 =  2.818e-13                 # cm
+
+#mass of elementary particle
+mc2 = 510.9989461                # keV
